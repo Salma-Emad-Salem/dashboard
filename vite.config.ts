74d1834dac876b30ecb16632ba601ad1a4e7ca13
Salma Emad <153438569+Salma-Emad-Salem@users.noreply.github.com>
2024-06-25 00:50:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: [
-        '@mui/material/styles'
-      ]
-    }
-  }
+    outDir: 'dist'
+  },
+  base: '/'
 });
